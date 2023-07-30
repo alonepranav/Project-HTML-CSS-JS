c7035decd67f1792244cd980414ab0a02b0cb828
= <=>
2023-07-30 13:39:50 +0530

    Practice website 8 added

---
+++ practice-website-8/index.js
@@ -0,0 +1,10 @@
+
+const open_close = () => {
+    const ul = document.querySelector("nav ul");
+    const i = document.querySelector("nav i");
+    ul.classList.toggle("show")
+    i.classList.toggle("bi-x-lg")
+}
+
+const submitForm = async () => {
+}
